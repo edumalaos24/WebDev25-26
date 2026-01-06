e52507a41e1466cd379ea10280a6a9cc88483c81
edumalaos24 <ethand390@nycstudents.net>
2026-01-06 19:06:10 +0000

    js

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function messi1(){
+    document.getElementById("highlight").innerHTML = "This is the GOAT";
+}
+
+function messi2(){
+    document.getElementById("highlight").innerHTML = "This is Lionel Messi";
+}
+
+function messi3(){
+    document.getElementById("highlight").innerHTML = "This is my GOAT";
+}
+
+function messi4(){
+    document.getElementById("highlight").innerHTML = "This is Messi";
+}
+
+function messi5(){
+    document.getElementById("highlight").innerHTML = "Bicycle kick from the goat";
+}

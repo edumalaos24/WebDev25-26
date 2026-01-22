52040d2fead95e6d03e8da9698ea04f5702056d3
edumalaos24 <ethand390@nycstudents.net>
2026-01-22 18:56:18 +0000

    jkl

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -15,7 +15,7 @@ function avg(){
 function slope(){
     let x1 = parseFloat(document.getElementById("x1").value);
     let y1 = parseFloat(document.getElementById("y1").value);
-    let x2 = parseFloat(document.getElemenyById("x2").value);
+    let x2 = parseFloat(document.getElementById("x2").value);
     let y2 = parseFloat(document.getElementById("y2").value);
     let op2 = document.getElementById("output2");
     let m = (y2 - y1)/(x2 - x1);
@@ -23,10 +23,10 @@ function slope(){
 }
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
 
-function BMI(){
-    let weight = parseFloat(document.getElementById("weight").value);
-    let height = parseFloat(document.getElementById("height").value);
+function bmi(){
+    let weight = parseInt(document.getElementById("weight").value);
+    let height = parseInt(document.getElementById("height").value);
     let op3 = document.getElementById("output3");
-    let BMI = 703*(weight/height);
-    op3.innerHTML = `Your BMI is ${BMI}`;
+    let bmiresult = (703*weight)/height**2;
+    op3.innerHTML = `Your BMI is ${bmiresult}`;
 }

765aad60521f5ba783a6a28dcbfa4c71f899dd65
edumalaos24 <ethand390@nycstudents.net>
2026-03-12 18:00:20 +0000

    5.2

--- WDUnit5/WDLesson5_2Practical/script.js
+++ WDUnit5/WDLesson5_2Practical/script.js
@@ -5,18 +5,27 @@ Notes:
    b. When creating the images give each a class of either "title" or "pizza" respectively
    c. Each image has been conveniently labeled with a number at the end in order to use a 
       loop to retrieve the images.
-2) Create an appropriate random price to display for each pizza
-3) Enter quantity is a text input
-4) Buy is a button input
+2) Create an appropriate random price to display for each pizza 15 to 35. No HTML needed.
+4) Enter quantity is a text input. No need to give it an ID.
+5) Buy is a button input
 
 Now that you have the template, use a for loop to produce all five pizza cards.
 */ 
-
 // Go to style.css for Challenge 3
 
 //init() function is called when the page loads in index.html
 function init(){
-  
-
+   let output = document.getElementById("output")
+   let build = "";
+   let i = "";
+   for (let i=1; i<6;i++){
+      let price = Math.floor(Math.random()*(35-15+1)+15);
+      build += `<div class = "card">
+      <img src= "images/title${i}.png" class = "title">
+      <img src = "images/pizza${i}.jpg" class = "pizza">
+      $${price}<input type = "text" placeholder = "Enter quantity">
+      <input type = "button" value = "Buy">
+      </div>`
+   }
+   output.innerHTML = build;
 }
-
